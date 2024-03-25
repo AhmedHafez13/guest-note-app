698aa089369d27f0ad482d61979faec3ef951ca4
Ahmed Hafez <ahmedhafez13121@gmail.com>
2024-03-25 23:01:09 +0200

    Update user schema

--- prisma/migrations/20240325180913_initial_schemas/migration.sql
+++ prisma/migrations/20240325180913_initial_schemas/migration.sql
@@ -4,12 +4,14 @@ CREATE TABLE `User` (
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `username` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
     `password` TEXT NOT NULL,
     `profilePicture` VARCHAR(255) NULL,
     `firstName` VARCHAR(255) NULL,
     `lastName` VARCHAR(255) NULL,
 
     UNIQUE INDEX `User_username_key`(`username`),
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
